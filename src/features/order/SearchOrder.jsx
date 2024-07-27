ad77784835530a7627c6ce76a87814550f004c76
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-27 07:32:51 -0700

    Styling Form Elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,6 +16,13 @@ function SearchOrder() {
                 placeholder="Insert Id"
                 onChange={(e) => setQuery(e.target.value)}
                 value={query}
+                className="rounded-full 
+                        px-4 py-2 text-sm
+                         bg-yellow-100
+                         placeholder:text-stone-400 
+                         sm:w-64 sm:focus:w-72 transition-all duration-300
+                         focus:outline-none focus:ring focus:ring-yellow-500 
+                         focus:ring-opacity-50"
             />
         </form>
     )
