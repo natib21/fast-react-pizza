3e3b404895f61e0f32d0731075c3a12fd5ba285e
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-09-09 01:27:18 -0700

    Add @Layer and @apply components to perform reusabiliy of style in index.css

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,13 +48,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required  className="input"/>
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required  className="input"/>
           </div>
           {formError?.phone && <p>{formError.phone}</p>}
         </div>
@@ -63,11 +63,7 @@ function CreateOrder() {
           <label>Address</label>
           <div>
             <input 
-            className="rounded-full border
-                        border-stone-200 px-4 py-2 text-sm transition-all
-                         duration-300 placeholder: text-stone-400 
-                         focus:outline-none
-                         focus:ring focus:ring-yellow-400 w-full md:px-6 md:py-3" type="text" name="address" required />
+            className="input" type="text" name="address" required />
           </div>
         </div>
 
